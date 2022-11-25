18e3d03e3f604fa19c4697c6786b1cb2df64e65e
WaleedAlsaeed <waleed.gdev+gbh@gmail.com>
2022-11-25 15:16:04 +0300

    AAA

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,13 +16,27 @@ exports.config = exports.client = void 0;
 require('dotenv').config();
 const Client_1 = require("./structures/Client");
 const consts_1 = require("./config/consts");
+const axios_1 = __importDefault(require("axios"));
 exports.client = new Client_1.ExtendedClient();
 exports.config = new consts_1.Config();
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
-    res.send("Hi");
     res.status(200).send('OK');
 });
 app.listen(port, () => exports.client.start());
+setInterval(() => __awaiter(void 0, void 0, void 0, function* () {
+    for (let i = 0; i < 30; i++) {
+        try {
+            const { data, status } = yield axios_1.default.get("https://wa-docs.onrender.com");
+            console.log(data);
+            console.log('response status is: ', status);
+            return;
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+    exports.config.LogChannel("Unable to check updates");
+}), 900000);
--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,25 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-    res.send("Hi")
     res.status(200).send('OK');
 });
 
 app.listen(port, () => client.start());
+
+
+
+setInterval(async () => {
+    for (let i = 0; i < 30; i++) {
+        try {
+            const { data, status } = await axios.get(
+                "https://wa-docs.onrender.com"
+            );
+            console.log(data);
+            console.log('response status is: ', status);
+            return;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    config.LogChannel("Unable to check updates");
+}, 900000)
